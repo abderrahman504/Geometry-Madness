f01226ec109794c315de3e1205d3782ad49c675b
abderrahman504 <rahmankhsherb@gmail.com>
2025-01-24 00:30:41 +0200

    Change using tweens in AmmoBars.gd

--- Scenes&Scripts/UI/AmmoBars.gd
+++ Scenes&Scripts/UI/AmmoBars.gd
@@ -12,32 +12,25 @@ enum BASETINTS {Blue, Red, Yellow}
 
 
 func _ready():
-	tween = GlobalReferences.tween
 	bar1 = $Bar1
 	bar2 = $Bar2
 
 
 func update_ammo_bar():
 	bar1.tint_progress = GlobalReferences.colours[gun1.gunType]
+	if tween.is_running():
+		tween.kill()
+	tween = create_tween()
 	tween.interpolate_property(bar1, "value", bar1.value, gun1.ammoCount, 0.2)
 	tween.interpolate_property(bar2, "value", bar2.value, 0, 0.2)
-	if not tween.is_active():
-		tween.start()
-
 
 
 func update_mixed_ammo_bar():
 	bar1.tint_progress = GlobalReferences.colours[gun1.gunType]
 	bar2.tint_progress = GlobalReferences.colours[gun2.gunType]
-	
+	if tween.is_running():
+		tween.kill()
+	tween = create_tween()	
 	tween.interpolate_property(bar1, "value", bar1.value, gun1.ammoCount, 0.2)
 	tween.interpolate_property(bar2, "value", bar2.value, gun2.ammoCount, 0.2)
-	if not tween.is_active():
-		tween.start()
-
-
-
-
-
-
 
