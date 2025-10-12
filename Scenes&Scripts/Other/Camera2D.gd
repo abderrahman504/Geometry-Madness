994e60282cea1a9a61fa5c0134e51f94585771c2
abderrahman504 <rahmankhsherb@gmail.com>
2025-10-12 17:23:17 +0300

    Fix janky camera movement

--- Scenes&Scripts/Other/Camera2D.gd
+++ Scenes&Scripts/Other/Camera2D.gd
@@ -4,6 +4,6 @@ extends Camera2D
 @export_range(0,1, 0.1) var cam_center_ratio : float = 0.2
 
 
-func _process(_delta):
+func _physics_process(_delta):
 	if GlobalReferences.playerExists:
 		position = GlobalReferences.player.position + (get_global_mouse_position() - GlobalReferences.player.position) * cam_center_ratio
