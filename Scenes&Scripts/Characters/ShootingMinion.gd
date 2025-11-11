5e6a07a7bd8dabbd6fe9c8507d648d3c6e5e7858
abderrahman504 <rahmankhsherb@gmail.com>
2025-11-11 15:20:50 +0200

    Create a simple shooting minion

---
+++ Scenes&Scripts/Characters/ShootingMinion.gd
@@ -0,0 +1,43 @@
+extends BaseEnemy
+class_name ShootingPursuer
+
+## Moves towards the player and shoots him.
+## The shooting pursuer does not drop a gun on death
+
+
+var desired_player_range : float = 50
+
+
+func _ready() -> void:
+	super._ready()
+	enemyType = -1
+	gun.user = self
+
+
+func handle_movement(delta):
+	if not GlobalReferences.playerExists:
+		return
+	
+	var dist_to_player := position.distance_to(GlobalReferences.player.position)
+	if dist_to_player > desired_player_range:
+		velocity = (GlobalReferences.player.position - position).normalized()
+		velocity *= max_speed
+	else:
+		velocity = Vector2.ZERO
+	
+	move_and_slide()
+	
+
+
+func handle_shooting(delta):
+	super.handle_shooting(delta)
+	
+	
+
+
+
+
+func drop_gun():
+	return
+
+
