f360f91313e223c8ee8e75cc3bde763d9a80f1e2
abderrahman504 <rahmankhsherb@gmail.com>
2025-12-18 14:40:30 +0200

    Some fixes Add all particle effect scenes to the world scene so they don't freeze the game when they first spawn. Fix an issue with `EaserIn` struggling to determine that the destination has been reached.

--- Scenes&Scripts/World/Camera/Positioner/EaserIn.gd
+++ Scenes&Scripts/World/Camera/Positioner/EaserIn.gd
@@ -5,15 +5,17 @@ class_name EaserIn
 signal destination_reached
 
 @export var rapidity : float = 20
+## Specifies how much time the easing function should assume has passed since it started.
 var start_time : float = 2
 var _time : float = start_time
 
 
 
 func get_desired_position(prev_position : Vector2, delta : float) -> Vector2:
-	if target.global_position.is_equal_approx(prev_position):
-		_time = start_time
-		destination_reached.emit()
 	_time += delta
 	var speed = pow(rapidity, _time)
-	return target.global_position.move_toward(prev_position, delta * speed)
+	var out := target.global_position.move_toward(prev_position, delta * speed)
+	if target.global_position.distance_to(prev_position) <= speed * delta:
+		_time = start_time
+		destination_reached.emit()
+	return out
